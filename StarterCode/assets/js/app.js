1605749702ab7432158d2b5bbec2d6221c83fb79
evrimpolat93 <77889795+evrimpolat93@users.noreply.github.com>
2021-05-13 21:27:41 -0400

    I added a new file

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -17,3 +17,19 @@ var svg = d3.select("#scatter").append("svg").attr("width", svgWidth)
 
 // create variable to hold svg elements
 const chartGroup = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`)
+
+// default values for x and y axises 
+var xkey = "poverty";
+var ykey = "healthcare";
+
+
+// function to put ticks values in x axis with the name of database 
+function xAxisScale(journalData, xkey) {
+    xScale = d3.scaleLinear()
+        .domain([d3.min(journalData, d => d[xkey]) * 0.8
+            , d3.max(journalData, d => d[xkey]) * 1.2])
+        .range([0, chartWidth])
+    return xScale
+
+}
+
